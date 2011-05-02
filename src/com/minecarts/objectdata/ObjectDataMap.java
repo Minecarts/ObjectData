d997c793d7d81f5d923da3f8f1e35174ec581b85
holic <kingersoll@gmail.com>
2011-05-02 16:29:38 -0700

    Flipped the map around to hopefully be more memory efficient

--- src/com/minecarts/objectdata/ObjectDataMap.java
+++ src/com/minecarts/objectdata/ObjectDataMap.java
@@ -1,67 +1,46 @@
 package com.minecarts.objectdata;
 
-import java.util.WeakHashMap;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map.Entry;
+import java.util.WeakHashMap;
 
 public class ObjectDataMap {
     
-    WeakHashMap<Object, HashMap<String, Object>> map;
+    HashMap<String, WeakHashMap<Object, Object>> map;
     
     public ObjectDataMap() {
-        map = new WeakHashMap<Object, HashMap<String, Object>>();
+        map = new HashMap<String, WeakHashMap<Object, Object>>();
     }
     
     public Object get(Object obj, String key) {
-        HashMap<String, Object> data = map.get(obj);
-        return data == null ? null : data.get(key);
-    }
-    
-    public HashMap<String, Object> getAll(Object obj) {
-        return map.get(obj);
+        WeakHashMap<Object, Object> data = map.get(key);
+        return data == null ? null : data.get(obj);
     }
     
     public ObjectDataMap set(Object obj, String key, Object value) {
-        HashMap<String, Object> data = map.get(obj);
+        WeakHashMap<Object, Object> data = map.get(key);
         
         if(data == null) {
-            data = new HashMap<String, Object>();
-            map.put(obj, data);
+            data = new WeakHashMap<Object, Object>();
+            map.put(key, data);
         }
         
-        data.put(key, value);
+        data.put(obj, value);
         
-        return this;
-    }
-    
-    public ObjectDataMap setAll(Object obj, HashMap<String, Object> data) {
-        map.put(obj, data);
         return this;
     }
     
     public ObjectDataMap remove(Object obj, String key) {
-        HashMap<String, Object> data = map.get(obj);
-        if(data != null) data.remove(key);
-        return this;
-    }
-    
-    public ObjectDataMap removeAll(Object obj) {
-        map.remove(obj);
+        WeakHashMap<Object, Object> data = map.get(key);
+        if(data != null) data.remove(obj);
         return this;
     }
     
     public WeakHashMap<Object, Object> withKey(String key) {
-        WeakHashMap<Object, Object> result = new WeakHashMap<Object, Object>();
-
-        for(Iterator<Entry<Object, HashMap<String, Object>>> entries = map.entrySet().iterator(); entries.hasNext();) {
-            Entry<Object, HashMap<String, Object>> entry = entries.next();
-            if(entry.getValue().containsKey(key)) {
-                result.put(entry.getKey(), entry.getValue().get(key));
-            }
-        }
-        
-        return result;
+        return map.get(key);
+    }
+    
+    public String[] keys() {
+        return map.keySet().toArray(new String[0]);
     }
     
 }
